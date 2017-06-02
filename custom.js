d5b0bb665d2a0c8a05a165d6ea28ebd8f58b2409
Pascal Lola <lola.pascal@gmail.com>
2017-06-01 22:54:50 -0500

    no message

--- custom.js
+++ custom.js
@@ -266,7 +266,7 @@ window.onload = function () {
     //initializeLineChart();
     //initializeHeatmap();
     var tempArray, density;
-    dateArray = getDates("04/17/17", "05/31/17", "MM.DD.YY");
+    dateArray = getDates("04/17/17", "05/30/17", "MM.DD.YY");
     hoursArray = ['12 AM', '01 AM', '02 AM', '03 AM', '04 AM', '05 AM', '06 AM', '07 AM', '08 AM', '09 AM', '10 AM', '11 AM', '12 PM', '01 PM', '02 PM', '03 PM', '04 PM', '05 PM', '06 PM', '07 PM', '08 PM', '09 PM', '10 PM', '11 PM'];
 
     $.ajax({
